1bbba5dd78ff801da39e84877ed0d5d80cf01211
Colgate13 <gabreilbarros13@gmail.com>
2021-03-06 16:26:56 -0300

    TEste

--- src/index.js
+++ src/index.js
@@ -2,10 +2,40 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ filhaDaPuta: "GAY VIADO" })
+app.use(express.json());
+
+app.get("/projects", (request, response) => {
+  const {title , owner} = request.query;
+
+  console.log(title);
+  console.log(owner)
+  return response.json({ Projeto: "Lili" })
 })
 
+app.post("/projects", (request, response) => {
+  const body = request.body;
 
-app.listen(3330, () => console.log("🧛‍♀️> Server is running!"));
+  const { title } = request.body;
+
+  const owener_body = request.body.owner;
+  console.log(body)
+
+  return response.json({ body: body, "Title": `${title}`, 'Owner': `${owener_body}`  })
+})
+
+app.put("/projects/:id", (request, response) => {
+  const params = request.params; // PEGA todos os parametros passados
+
+  const params_id = request.params.id; // PEGA SO O PARAMETRO ID
+
+  const { id } = request.params; // PEGA SO O PARAMETRO ID de um jeito diferente
+
+  console.log(params_id)
+  return response.json({ params: `${id}` })
+})
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json({ REMOVEEEEEE: "Projeto: Lili" })
+})
+app.listen(3330, () => console.log("🧛‍♀️ > Server is running! 🧡Eu amo a Lili💖"));
  
