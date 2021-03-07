8d52d3f795d5b7081243837f4c1b0f53f9917b5f
Colgate13 <gabreilbarros13@gmail.com>
2021-03-07 20:47:33 -0300

    Penultima aula

---
+++ src/ParametrosIMPORTANTE.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.get("/projects", (request, response) => {
+  const {title , owner} = request.query;
+
+  return response.json(projects)
+})
+
+app.post("/projects", (request, response) => {
+  const body = request.body;
+
+  const { title } = request.body;
+
+  const owener_body = request.body.owner;
+  console.log(body);
+
+  return response.json({ body: body, "Title": `${title}`, 'Owner': `${owener_body}`  })
+})
+
+app.put("/projects/:id", (request, response) => {
+  const params = request.params; // PEGA todos os parametros passados
+
+  const params_id = request.params.id; // PEGA SO O PARAMETRO ID
+
+  const { id } = request.params; // PEGA SO O PARAMETRO ID de um jeito diferente
+
+  console.log(params_id)
+  return response.json({ params: `${id}` })
+})
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json({ REMOVEEEEEE: "Projeto: Lili" })
+})
+app.listen(3330, () => console.log("🧛‍♀️ > Server is running! 🧡Eu amo a Lili💖"));
+ 
--- src/index.js
+++ src/index.js
@@ -1,41 +1,71 @@
 const express = require('express');
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/projects", (request, response) => {
-  const {title , owner} = request.query;
 
-  console.log(title);
-  console.log(owner)
-  return response.json({ Projeto: "Lili" })
+const projects = [];
+
+
+app.get("/projects", (request, response) => {
+  const {title } = request.query;
+
+  const results = title
+  ? projects.filter(project => project.title.includes(title))
+  : projects
+
+  return response.json(results)
 })
 
 app.post("/projects", (request, response) => {
-  const body = request.body;
+  const {title , owner} = request.body;
+  
+  const project = { id: uuid(),  title, owner };
 
-  const { title } = request.body;
+  projects.push(project);
 
-  const owener_body = request.body.owner;
-  console.log(body);
-
-  return response.json({ body: body, "Title": `${title}`, 'Owner': `${owener_body}`  })
+  return response.json(project)
 })
 
 app.put("/projects/:id", (request, response) => {
-  const params = request.params; // PEGA todos os parametros passados
+  const { id } = request.params; // PEGA todos os parametros passados
+  const {title , owner} = request.body;
 
-  const params_id = request.params.id; // PEGA SO O PARAMETRO ID
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-  const { id } = request.params; // PEGA SO O PARAMETRO ID de um jeito diferente
+  if(projectIndex < 0)
+  {
+    return response.status(400).json( { error: 'Project not found' } )
+  }
 
-  console.log(params_id)
-  return response.json({ params: `${id}` })
+  const project = {
+    id,
+    title,
+    owner,
+  };
+  
+  projects[projectIndex] = project;
+
+  return response.json(project)
 })
 
 app.delete("/projects/:id", (request, response) => {
-  return response.json({ REMOVEEEEEE: "Projeto: Lili" })
+
+  const { id } = request.params; // PEGA todos os parametros passados
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0)
+  {
+    return response.status(400).json( { error: 'Project not found' } )
+  }
+
+  projects.splice(projectIndex, 1);
+
+
+  return response.status(204).send()
 })
 app.listen(3330, () => console.log("🧛‍♀️ > Server is running! 🧡Eu amo a Lili💖"));
  
